c77623ec53a292d48d5d2b431c18e66da9431740
Camden Van Sciver <camdenvs02@gmail.com>
2023-07-04 15:03:40 -0700

    added a color theme for site styling

--- app/src/App.js
+++ app/src/App.js
@@ -1,8 +1,12 @@
 import React from 'react'
 
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+
 function App() {
   return (
-    <></>
+    <Router>
+      
+    </Router>
   );
 }
 
